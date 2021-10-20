b9e87ac55ceda8fae63b08ba1c54840e434e29f8
CorneliusJazz <davidrincongz@gmail.com>
2021-10-19 19:57:54 -0500

    [UPDATE] Correccion de Repositorio Ventas

--- src/main/java/principal/repositories/VentasRepository.java
+++ src/main/java/principal/repositories/VentasRepository.java
@@ -1,11 +1,12 @@
 package principal.repositories;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import principal.models.VentasModel;
 
 @Repository
-public interface VentasRepository {
+public interface VentasRepository extends JpaRepository<VentasModel, Long> {
 
     VentasModel findByCodigoVenta(Long codigoVenta);
 
